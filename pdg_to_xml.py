5f0f1d1c88954b76c49ae60608a2b23d192736a3
Eduardo Benavidez <eduardob@slac.stanford.edu>
2012-05-15 17:27:48 -0700

    Refactored whole script

--- pdg_to_xml.py
+++ pdg_to_xml.py
@@ -4,202 +4,209 @@ import time
 import urllib
 import urllib2
 import simplejson as json
-#from invenio.search_engine import perform_request_search as search
 
 """
 Read the pdg_codes file to produce a marcxml file.
 Each record is verified that it exists on INSPIRE (find j PRLTA,54,2489),
 and the record id is used in the marcxml file.
-From: PRLTA,54,2489,S035:Desig=20,S035:Desig=3,S035:Desig=4
+From: PHRVA,D32,2468,S035RHE,S035RHM,S035RHO
 To:
-<record>
-<controlfield tag='001'>213362</controlfield>
-<datafield tag='037'><subfield code='z'>s035:desig=20</subfield></datafield>
-<datafield tag='037'><subfield code='z'>s035:desig=3</subfield></datafield>
-<datafield tag='037'><subfield code='z'>s035:desig=4</subfield></datafield>
-</record>
-Record syntax istill to be verified!!!
+ <record>
+    <controlfield tag='001'>221960</controlfield>
+    <datafield tag='084'>
+       <subfield code='a'>s035rhe</subfield>
+       <subfield code='2'>PDG</subfield>
+       <subfield code='9'>PDG</subfield>
+    </datafield>
+    <datafield tag='084'>
+       <subfield code='a'>s035rhm</subfield>
+       <subfield code='2'>PDG</subfield>
+       <subfield code='9'>PDG</subfield>
+    </datafield>
+    <datafield tag='084'>
+       <subfield code='a'>s035rho</subfield>
+       <subfield code='2'>PDG</subfield>
+       <subfield code='9'>PDG</subfield> 
+    </datafield>
+ </record>
 """
-
-
-def move_around_letters(journal,volume,pages):
+#************ GLOBALS ********************
+#INSPIRE_URL = 'http://inspirehep.net/search?'
+INSPIRE_URL = 'http://inspireheptest.cern.ch/search?'
+PDG_FILE    = 'pdg_sm.csv'
+  
+def parse_fields(row):   
     """
-Move around the letter attached of a volume or page ref seeking unique
-
-Sometimes volume letters for a coden reference get put in the wrong place:
-they're on the page when they should be on the volume, or they're at the
-front of the volume when they should be at the back (or vice versa).
-Here we try moving the letters around until we've gotten exactly 0 hits for
-various permutations or we find a unique result.
-
-If we find no unique result, throw the volume letter away altogether.
-"""
-
-    letters = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    letter = ''
-    hits = []
+    if row starts with #, journal is returned as an empty string
     
-    if not volume:
-        #nothing todo
-        return hits
-    
-    def permutations(journal,volume,pages,letter):
-        yield journal,letter+volume,pages
-        yield journal,volume+letter,pages
-        yield journal,volume,letter+pages
-        yield journal,volume,pages+letter
-
-    
-    
-    if volume[0] in letters:
-        letter = volume[0]
-        volume = volume[1:]
-    elif volume[-1] in letters:
-        letter = volume[-1]
-        volume = volume[:-1]
-    elif pages[0] in letters:
-        letter = pages[0]
-        pages = pages[1:]
-    elif pages[-1] in letters:
-        letter = pages[-1]
-        pages = pages[:-1]
-        
-    if letter != '':
-        for j,v,p in permutations(journal,volume,pages,letter):
-            hits = get_inspire_id(j,v,p)
-            if len(hits) > 0:
-                return hits
-            
-    return hits
-        
-
-def get_inspire_id(journal,volume,pages):
     """
-Search in inspirehep.net for record id
-Return a list of one or more ids that match with journal, volume, and pages
-"""
+             
+    journal = row[0].strip().upper()                
+    if journal.startswith('#'): # skip comments
+        volume = pages = journal = ''
+        codes = []    
+    else:
+        volume, pages = [x.strip().upper() for x in row[1:3]]
+        codes = [x.strip().lower() for x in row[3:]]
+        
+    return journal, volume, pages, codes
+#-----------------------------------------------------------------------------        
+      
+def get_search_url(journal, volume, pages):
+    """
+    Get INSPIRE search URL (url-encoded)
+    
+    If volume or pages is blank, it is assumed an irn of the form    
+      http://inspirehep.net/search?p=find%20irn%204034872&of=id
+    Otherwise, it's taken as a journal
+      http://inspirehep.net/search?p=find%20j%20PHLTA,14,105&of=id
+    
+    Return the url encoded url with output format set to id
+    """
+    global INSPIRE_URL
+    
     if volume or pages:    
-        search_str = 'find j ' + ','.join([journal,volume,pages])
+        search_str = 'find j ' + ','.join([journal, volume, pages])
     else:
         search_str = 'find irn ' + journal
-    invenio_url = 'http://inspirehep.net/search?'
-    data = {'p': search_str, 'of': 'id'}
-    hits_handle = urllib2.urlopen(invenio_url + urllib.urlencode(data))
+        
+    return INSPIRE_URL + urllib.urlencode({'p': search_str, 'of': 'id'})
+#-----------------------------------------------------------------------------    
+              
+def get_inspire_id(journal, volume, pages):
+    """
+    Search in inspirehep.net for record id(s)  based on journal, volume and pages
+       
+    Return record id list (empty list of none found) and the manipulate flag 
+    to notify if volume letter was in an unexpected place. 
+    """
+    letters = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    letter = ''    
+    manipulate = False
+    
+    #no need to look for volume letter on pages. PHRVA,D43,R1,s066crd
+    def permutations(volume,letter):
+        yield letter+volume
+        yield volume+letter
+        
+
+    search_url = get_search_url(journal, volume, pages)
+    #print search_url
+    
+    #try without manipulation
+    hits_handle = urllib2.urlopen(search_url)
     hits = json.loads(hits_handle.read())
     
-    return hits
-    
-    
-def get_ref_hits_codes(line):
+    if len(hits) == 0:
+        #try volume permutations
+        manipulate = True            
+        if volume:
+            if volume[0] in letters:
+                letter = volume[0]
+                volume = volume[1:]
+            elif volume[-1] in letters:
+                letter = volume[-1]
+                volume = volume[:-1]
+            
+            if letter != '':
+                for vol in permutations(volume,letter):
+                    search_url = get_search_url(journal, vol, pages)                      
+                    hits_handle = urllib2.urlopen(search_url)
+                    hits = json.loads(hits_handle.read())
+                    if len(hits) > 0:
+                        break       
+            
+    return hits,manipulate
+#-----------------------------------------------------------------------------    
+
+def get_marc_record(record_id, codes):
     """
-Return a search, its result set, and the set of PDG codes to attach
+    create a MARC 21 record using the record id and the PDG codes
+    
+    """
+    
+    xml_str = ' <record>\n    <controlfield tag=\'001\'>' + record_id + '</controlfield>'
 
-Returns a tuple consisting of
-* hits - the set of records responsive to the reference search
-may be a LIST or NONE
-* codes - the collection of PDG code designations to be assigned to the
-unique record specified by hits. maybe a LIST of STRINGS or NONE
-* journal, volume, and pages
-"""
+    for code in codes:
+        xml_str = xml_str + '\n    <datafield tag=\'084\'>' + '\n       <subfield code=\'a\'>' + code + \
+                                    '</subfield>' + '\n       <subfield code=\'2\'>PDG</subfield>' + \
+                                    '\n       <subfield code=\'9\'>PDG</subfield>\n    </datafield>'
+    xml_str = xml_str + '\n </record>\n'
+    
+    
+    return xml_str
+#-----------------------------------------------------------------------------    
 
-    hits = None
-    journal = line[0].strip().upper()
-    if journal.startswith('#'): # skip comments
-        return None, None, None, None, None
+def write_to_file(file_name, save_str):
+    """
+    Write string to file
     
-    volume,pages = [x.strip().upper() for x in line[1:3]]
-    codes = [x.strip().lower() for x in line[3:]]
-    
-    #if volume or pages:
-    hits = get_inspire_id(journal,volume,pages)
-                    
-    return codes, hits, journal, volume, pages
-  
-  
-  
-  
-def main():
-
-    # read data from file, parsing w/ csv
-    pdg_list = csv.reader(open('pdg_sm.csv', 'rb'), delimiter=',', quotechar='"', skipinitialspace=True)
-    recs_found = 0
-    recs_missing = 0
-    recs_duplicates = 0
-    recs_volume_mix = 0
-    recs_total = 0
-    DEBUGCOUNT = 0    
-    marc_xml = open('marcxml.txt', 'wb')
-    errmissingfile = open('needed_a_massage.txt', 'wb')
-    notfoundfile = open('not_found.txt', 'wb')
-    xml_str = '<?xml version="1.0" encoding="UTF-8"?>\n' + '<collection xmlns="http://www.loc.gov/MARC21/slim">\n'
-    missing_str = '#MISSING ON FIRST TRY, SO HAD TO MOVE VOL LETTER\n'
-    notfound_str = ''
-    
-    print 'processing codeList...'
-    for pdg_line in pdg_list:
-        hits = None
-        hits_len = 0
-        DEBUGCOUNT += 1
-        if DEBUGCOUNT % 100 == 0:
-            time.sleep(random.randint(1,9))
-            print "100 records processed. This includes comment lines"
-    
-        codes, hits, journal, volume, pages = get_ref_hits_codes(pdg_line)
+    """    
+    fh = open(file_name, 'wb')            
+    fh.write(save_str)           
+    fh.close()
         
-        #When journal is None, line was a comment. Otherwise, count as record
-        if journal != None: recs_total = recs_total + 1
-        if hits:
+#-----------------------------------------------------------------------------
+             
+                
+def main():           
+    #Initialize    
+    DEBUGCOUNT   = 0 
+    hits         = []
+    manipulate   = False 
+    duplicates   = found = not_found = manipulate_count = 0
+    xml_str      = '<?xml version="1.0" encoding="UTF-8"?>\n' + '<collection xmlns="http://www.loc.gov/MARC21/slim">\n'  
+    manipulated_str = not_found_str = dups_str = '' #to save to a file
+    
+    print 'processing PDGs...'
+    pdg_reader   = csv.reader(open(PDG_FILE, 'rb'), delimiter=',', quotechar='"', skipinitialspace=True)
+    
+    #main loop        
+    for row_str_list in pdg_reader:                          
+        journal, volume, pages, codes = parse_fields(row_str_list)
+        current_row  = ''            
+        #if journal is blank, it's probably a comment
+        if journal:                        
+            DEBUGCOUNT += 1
+            if DEBUGCOUNT % 10 == 0:
+                print str(DEBUGCOUNT) + "records processed. Sleeping..."
+                time.sleep(random.randint(1, 9))
+            current_row =  current_row + journal + ',' + volume + ',' + pages + ',' + ','.join(codes) + '\n'
+            print current_row        
+            hits, manipulate = get_inspire_id(journal, volume, pages)           
             hits_len = len(hits)
-        if hits_len < 1:            
-            #count if not a comment
-            if journal != None: 
-                #Missing, so try again
-                missing_str = missing_str + ','.join(pdg_line) + '\n'
-                recs_volume_mix = recs_volume_mix + 1
-            hits = move_around_letters(journal,volume,pages)
-            if hits:
-                hits_len = len(hits)
-                print 'After second try. hits_len:' + str(hits_len) + '\n content: ' + str(hits[0])
-            if hits_len < 1:                
-                if journal != None: 
-                    #Missing again, so quit looking
-                    recs_missing = recs_missing + 1                
-                    print '*Missing:' + ','.join(pdg_line) + '\n'
-                    notfound_str = notfound_str + ','.join(pdg_line) + '\n'
-                    
-        if hits_len > 1:
-            print 'duplicate found:' + journal + ', ' + volume + ', ' + pages
-            recs_duplicates = recs_duplicates + 1
-        
-        if hits_len == 1:
-            print 'ok:' + str(hits[0])
-            xml_str = xml_str + ' <record>\n <controlfield tag=\'001\'>' + str(hits[0]) + '</controlfield>'
-
-            for code in codes:
-                xml_str = xml_str + '\n <datafield tag=\'084\'>' + '\n <subfield code=\'a\'>' + code + \
-                                    '</subfield>' + '\n <subfield code=\'2\'>PDG</subfield>' + \
-                                    '\n <subfield code=\'9\'>PDG</subfield>\n </datafield>'
-            xml_str = xml_str + '\n </record>\n'
-            recs_found = recs_found + 1
-    
+                        
+            if hits_len > 0:
+                if hits_len > 1:
+                    duplicates +=1    
+                    dups_str = dups_str + current_row            
+                else:
+                    #** one found ***
+                    found += 1                        
+                    xml_str = xml_str + get_marc_record(str(hits[0]), codes)          
+            else:
+                not_found += 1
+                not_found_str = not_found_str + current_row
+            
+            if manipulate: 
+                manipulate_count += 1
+                manipulated_str = manipulated_str + current_row
     
     xml_str = xml_str + '</collection>\n</xml>'
-    marc_xml.write(xml_str)
-    marc_xml.close()
     
-    errmissingfile.write(missing_str)    
-    errmissingfile.close()
-    notfoundfile.write(notfound_str)
-    notfoundfile.close()
+    #save files    
+    write_to_file('marcxml.txt', xml_str)
+    write_to_file('needed_a_massage.txt', manipulated_str)
+    write_to_file('not_found.txt', not_found_str)
+    write_to_file('dups.txt', dups_str)
     
-    print 'done processing. \n'
-    print 'Found: ' + str(recs_found) + '\n'
-    print 'Missing: ' + str(recs_missing) + '\n'
-    print 'Vol Manipulation: ' + str(recs_volume_mix) + '\n'
-    print 'Duplicates: ' + str(recs_duplicates) + '\n'
-    print 'Total: ' + str(recs_total) + '\n'
-        
-
+    #reporting...                        
+    print 'Found: ' + str(found)     
+    print 'Not Found: ' + str(not_found)
+    print 'Duplicates: ' + str(duplicates)     
+    print 'Manipulated: ' + str(manipulate_count)
+    print 'Total: ' + str(DEBUGCOUNT) + '\n'
+    print 'done'
 
 if __name__ == '__main__':
     main()
